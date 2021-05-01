ffdc5cd918d25485476b307bbe51e8b59751feb6
ozgursakizli <ozgursakizli@gmail.com>
2021-05-01 19:09:47 +0300

    SplashActivity

---
+++ app/src/main/java/com/ozgursakizli/starshipdelivery/ui/SplashActivity.kt
@@ -0,0 +1,43 @@
+package com.ozgursakizli.starshipdelivery.ui
+
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ozgursakizli.starshipdelivery.databinding.ActivitySplashBinding
+import timber.log.Timber
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.d("onCreate")
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.lottieAnim.animate().setListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+                Timber.d("onAnimationStart")
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                Timber.d("onAnimationEnd")
+                Intent(this@SplashActivity, MainActivity::class.java).apply {
+                    startActivity(this)
+                    finish()
+                }
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                Timber.d("onAnimationCancel")
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+                Timber.d("onAnimationRepeat")
+            }
+
+        }).alpha(0F).duration = 3000
+    }
+
+}
