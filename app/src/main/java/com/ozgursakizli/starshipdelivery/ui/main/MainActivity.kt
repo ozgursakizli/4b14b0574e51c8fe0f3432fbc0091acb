14c36a1a4638f6353f807a952f3b7fbab6cf6ce4
ozgursakizli <ozgursakizli@gmail.com>
2021-05-02 03:07:47 +0300

    MainActivity layout

--- app/src/main/java/com/ozgursakizli/starshipdelivery/ui/main/MainActivity.kt
+++ app/src/main/java/com/ozgursakizli/starshipdelivery/ui/main/MainActivity.kt
@@ -1,6 +1,19 @@
 package com.ozgursakizli.starshipdelivery.ui.main
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ozgursakizli.starshipdelivery.databinding.ActivityMainBinding
+import timber.log.Timber
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.d("onCreate")
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
 }
